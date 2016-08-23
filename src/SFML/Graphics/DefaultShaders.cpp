a954c67710e3a698ecad1a29cc378ca4c02e0d11
Sam Izzo <sam@humbug.net>
2016-08-23 18:09:35 +1000

    Added newlines, otherwise the shader compiler can't parse the shaders!

--- src/SFML/Graphics/DefaultShaders.cpp
+++ src/SFML/Graphics/DefaultShaders.cpp
@@ -5,38 +5,38 @@ namespace sf
 {
 
 static const char* DefaultFragmentShaderUntextured =
-"#version 120"
-"void main(void)"
-"{"
-"	gl_FragColor = gl_Color;"
+"#version 120\n"
+"void main(void)\n"
+"{\n"
+"	gl_FragColor = gl_Color;\n"
 "}";
 
 static const char* DefaultVertexShaderUntextured =
-"#version 120"
-"uniform vec4 u_colour;"
-"void main(void)"
-"{"
-"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;"
-"	gl_FrontColor = gl_Color * u_colour;"
+"#version 120\n"
+"uniform vec4 u_colour;\n"
+"void main(void)\n"
+"{\n"
+"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;\n"
+"	gl_FrontColor = gl_Color * u_colour;\n"
 "}";
 
 static const char* DefaultFragmentShaderTextured =
-"#version 120"
-"uniform sampler2D u_tex;"
-"void main(void)"
+"#version 120\n"
+"uniform sampler2D u_tex;\n"
+"void main(void)\n"
 "{"
-"	vec4 colour = texture2D(u_tex, gl_TexCoord[0].st);"
-"	gl_FragColor = gl_Color * colour;"
+"	vec4 colour = texture2D(u_tex, gl_TexCoord[0].st);\n"
+"	gl_FragColor = gl_Color * colour;\n"
 "}";
 
 static const char* DefaultVertexShaderTextured =
-"#version 120"
-"uniform vec4 u_colour;"
-"void main(void)"
-"{	"
-"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;"
-"	gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;"
-"	gl_FrontColor = gl_Color * u_colour;"
+"#version 120\n"
+"uniform vec4 u_colour;\n"
+"void main(void)\n"
+"{\n"
+"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;\n"
+"	gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;\n"
+"	gl_FrontColor = gl_Color * u_colour;\n"
 "}";
 
 static bool s_initialised = false;
